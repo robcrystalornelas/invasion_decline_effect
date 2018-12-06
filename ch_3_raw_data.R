9852ca10a7ecc3bb1c96f424606ec6fcc03eabc8
rpecchia <rpecchia@gmail.com>
2018-12-05 16:23:50 -0800

    renames and moving

--- ch_3_raw_data.R
+++ ch_3_raw_data.R
@@ -2,4 +2,4 @@
 library(dplyr)
 
 ## READ IN DATA ####
-raw_data <- read.csv("~/Desktop/CH3_impacts_meta_analysis/diversity_cases_working_file_v6.csv", header=TRUE)
+raw_data <- read.csv("~/Desktop/CH3_impacts_meta_analysis/diversity_cases_working_file_v7.csv", header=TRUE)
--- extracting_data_from_boxplots.R
+++ extracting_data_from_boxplots.R
@@ -1,3 +1,4 @@
 library(metaDigitise)
-
 metaDigitise(dir = "~/Desktop/CH3_impacts_meta_analysis/data_in_boxplots", summary = TRUE)
+?metaDigitise
+         
