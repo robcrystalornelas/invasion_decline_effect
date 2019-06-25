053da91bdf6cc96ff3045fffc34899ab1d6a73a6
rpecchia <rpecchia@gmail.com>
2019-06-25 13:43:14 -0700

    model and figure for SCImago Journal Rank

--- impact_factor_and_pub_year/impact_factor_and_publication_year.R
+++ impact_factor_and_pub_year/impact_factor_and_publication_year.R
@@ -8,33 +8,41 @@ library(ggthemes)
 library(metaviz)
 library(metafor)
 library(gridExtra)
+library(lme4)
 
 # Get subset of columns for CMA
 impact_factor_model <- select(raw_data_imputed, code, publicationyear,impactfactor)
 head(impact_factor_model)
 
 # make model
-linear_model_impact_factor <- lm(impactfactor ~ publicationyear, data=impact_factor_model)  # build linear regression model on full data
-
-# first, have to check if it's statistically significant
+linear_model_impact_factor <- lmer(impactfactor ~ publicationyear + (1|code), data=impact_factor_model)  # build linear regression model on full data
 summary(linear_model_impact_factor)
 
-# basic linear regression plot
-gg_impact_factor <- ggplot(impact_factor_model, aes(x=publicationyear, y=impactfactor)) + 
-  geom_smooth(method=lm) +
-  geom_jitter(shape = 1)
-gg_impact_factor
-gg_impact_factor <- gg_impact_factor + scale_x_continuous(name = "Publication Year") +
-   scale_y_continuous(name = "Impact factor")
-gg_impact_facotr <- gg_impact_factor + theme_cowplot() +
-  theme(axis.title = element_text(size = 15),
-        axis.text = element_text(size = 14)) +
-  geom_hline(yintercept=0, linetype="dashed", 
-             color = "black", size=.3)
-gg_impact_factor
-
-gg_impact_factor
+#### Creating figure for linear model and journal rank
+impact_factor_plot <- ggplot(data = impact_factor_model,
+                             aes(x = publicationyear,
+                                 y = impactfactor)) +
+  geom_point(size = .8,
+             alpha = .8,
+             position = "jitter") +
+  geom_smooth(
+    method = lm,
+    se = FALSE,
+    size = 1,
+    alpha = .8
+  ) +
+  theme_cowplot() +
+  ylab("SCImago Journal Rank") +
+  xlab("Publication year") +
+  theme(
+    axis.title = element_text(size = 15),
+    axis.text = element_text(size = 14),
+    legend.title = element_text(size = 14),
+    legend.text = element_text(size = 13)
+  )
+impact_factor_plot
 pdf(file="~/Desktop/CH3_impacts_meta_analysis/figures/linear_model_impact_factor.pdf")
-gg_impact_factor
+impact_factor_plot
 dev.off()
 dev.off()
+
